129833f42e6d36df6a594178ebd4b54545dbcd50
wwc <onechoo1106@gmail.com>
2023-10-04 18:57:41 +1000

    local storage test for netlify

---
+++ src/TimeToMoney/page.tsx
@@ -0,0 +1,39 @@
+import React, {useState,useEffect} from "react";
+
+
+const TextInput: React.FC = () => {
+    const [inputValue, setInputValue] = useState<string>('');
+    const [storedValue, setStoredValue] = useState<string | null>(null);
+  
+    // Retrieve the stored value from local storage when the component mounts
+    useEffect(() => {
+      const storedItem = localStorage.getItem('storedValue');
+      if (storedItem) {
+        setStoredValue(storedItem);
+      }
+    }, []);
+  
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value);
+    };
+  
+    const handleSubmit = () => {
+      // Store the input value in local storage
+      localStorage.setItem('storedValue', inputValue);
+      setStoredValue(inputValue);
+    };
+  
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter text"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+        {storedValue && <div>Stored Value: {storedValue}</div>}
+      </div>
+    );
+  };
+export default TextInput;
